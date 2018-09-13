b904b53a876508b8bc4c4e4cd65188444a9bf440
LAPTOP-2MB32HVA\NIDIA <nidia.gonzalezm08@gmail.com>
2018-09-12 20:25:55 -0600

    no message

---
+++ C++/EVA1_1_HOLAMUNDO.cpp
@@ -0,0 +1,15 @@
+/******************************************************************************
+
+Primera practica en c++. Definicion de nomenclatura para prácticas e introducción al ambiente
+de desarrollo (IDE)
+Nidia Gonzalez morales 18550676
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello World");
+
+    return 0;
+}
