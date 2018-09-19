62ca52b9ab95bdf04565ea5a3ea1a08209c9b083
LAPTOP-2MB32HVA\NIDIA <nidia.gonzalezm08@gmail.com>
2018-09-18 23:07:00 -0600

    Practicas

---
+++ Python/EVA1_10_CUNAESFERICA.py
@@ -0,0 +1,15 @@
+
+
+'Nidia Gonzalez Morales 18550676'
+
+print("Ingresa el radio")
+r = float(input())
+
+print("Ingresa los grados")
+g = float(input())
+
+vT = float(4/3)*(3.1416*r**3/360)*g
+
+print("El volumen de la cuna esferica es ")
+print(vT)
+
---
+++ Python/EVA1_11_FORMULAPENDIENTE.py
@@ -0,0 +1,19 @@
+
+
+'Nidia Gonzalez Morales 18550676'
+
+print("Ingresa el primer valor de x");
+x1=float(input())
+
+print("Ingresa el segundo valor de x");
+x2=float(input())
+
+print("Ingresa el primer valor de y");
+y1=float(input())
+
+print("Ingresa el segundo valor de y");
+y2=float(input())
+
+p= float((y2-y1)/(x2-x1))
+print("La pendiente es ")
+print(p)
